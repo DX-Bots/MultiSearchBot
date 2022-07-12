16b66bf3491e02052e1a7293ff03c99f6219ee89
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-07-12 18:22:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ import os
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
 
-from pyrogram import Client
-from pyrogram import filters
-from MultiSearchBot.callback import *
+from pyrogram import Client, filters
 from configs import Config
 
-Bot = Client(session_name=Config.SESSION_NAME, api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
+Bot = Client(
+    session_name=Config.SESSION_NAME,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN
+)
 
 Bot.run()
