4dbbfc5721fea4bb8b74e9b09ca5c2e589798f1f
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-07-12 12:18:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 # (c) @PredatorHackerzZ
 
 import asyncio
-from pyrogram import Client, filters
+from pyrogram import Client as Bot
+from pyrogram import filters
 from pyrogram.errors import QueryIdInvalid, FloodWait
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, InlineQuery, CallbackQuery, InlineQueryResultArticle, \
     InputTextMessageContent
-from MultiSearchBot.scripts import Script
 from MultiSearchBot.callback import *
 from configs import Config
-from tool import SearchYTS, SearchAnime, Search1337x, SearchPirateBay, SearchPyPi
 
 Bot = Client(session_name=Config.SESSION_NAME, api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
-
-@Bot.on_message(filters.command("start"))
-async def start_handler(_, message: Message):
-    try:
-        await message.reply_text(
-            text=Script.START_TEXT.format(message.from_user.mention),
-            disable_web_page_preview=True,
-            parse_mode="html",
-            reply_markup=Script.START_BUTTONS
-        )
-    except FloodWait as e:
-        print(f"[{Config.SESSION_NAME}] - Sleeping for {e.x}s")
-        await asyncio.sleep(e.x)
-        await start_handler(_, message)
 
 Bot.run()
